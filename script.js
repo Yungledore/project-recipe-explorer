abb3225a8fcbb7602a2d5d8bed766be61ec88778
Yungledore <Farisnukman2011@gmail.com>
2026-04-02 18:44:56 +0800

    wait

--- script.js
+++ script.js
@@ -147,6 +147,67 @@ document.addEventListener("click", function (event) {
     fetchDetails(id);
   }
 });
+function showModal(meal) {
+  // build ingredients list
+
+  let ingredientsList = "";
+  for (let i = 1; i <= 20; i++) {
+    const ingredient = meal["strIngredient" + i];
+    const measure = meal["strMeasure" + i];
+    if (ingredient && ingredient.trim() !== "") {
+      ingredientsList +=
+        "<li>" + ingredient + (measure ? " - " + measure : "") + "</li>";
+    }
+  }
+  let youtubeHtml = "";
+  if (meal.strYoutube) {
+    youtubeHtml = `<p class="mt-2">
+      <strong>Watch on YouTube:</strong>
+      <a href="${meal.strYoutube}" target="_blank" rel="nooperner noreferrer">Click here</a>
+    </p>`;
+  }
+  const modalBody = document.getElementById("modalBody");
+  modalBody.innerHTML = `
+  <div class="row g-3">
+  <div class="col-md-5">
+  <img src="${meal.strMealThumb}" alt="${meal.strMeal}" class="img-fluid rounded mb-2">
+  <p class="mb-1"><strong>Area:</strong>${meal.strArea || "World"}</p>
+  <p class="mb-1"><strong>Category:</strong>${meal.strCategory || "Dish"}</p>
+  </div>  
+  <div class="col-md-7">
+  <h4 class="fw-bold mb-2">${meal.strMeal}</h4>
+  <h6 class="fw-semibold">Ingredients</h6>
+  <ul class="mb-3">
+  ${ingredientsList}
+  </ul>
+  <h6 class="fw-semibold">Instructions</h6>
+  <div class="instructions-box">
+  ${meal.strInstructions}
+  </div>
+  ${youtubeHtml}
+  </div>
+  </div>
+  `;
+  function toggleFavourite(id, title) {
+    const btn = document.getElementById("favBtn");
+
+    if (favourites[id]) {
+      // If it exists, remove it
+      delete favourites[id];
+      btn.innerText = "Mark as Favourite";
+      btn.classList.replace("btn-danger", "btn-outline-danger");
+    } else {
+      // If not, add it with default empty notes
+      favourites[id] = { title: title, notes: "" };
+      btn.innerText = "Remove Favourite";
+      btn.classList.replace("btn-outline-danger", "btn-danger");
+    }
+
+    syncStorage();
+  }
+  const modal = new bootstrap.Modal(document.getElementById("recipeModal"));
+  modal.show();
+}
 document.addEventListener("click", function (event) {
   if (event.target.classList.contains("view-btn")) {
     const id = event.target.getAttribute("data-id");
